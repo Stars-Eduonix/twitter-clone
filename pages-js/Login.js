140a6003680e7dc4c3fee4830621686d28b182ee
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-05-06 20:08:38 +0530

    login and signup, google login

--- pages-js/Login.js
+++ pages-js/Login.js
@@ -1,14 +1,31 @@
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import firebaseApp from "../firebase.js";
 
 const auth = getAuth(firebaseApp)
 
-function signIn(email, password){
+const provider = new GoogleAuthProvider();
+
+
+let signInForm = document.querySelector(".signIn-form");
+let googleButton = document.querySelector(".google-login");
+
+signInForm.addEventListener("submit", signIn)
+
+function signIn(e){
+    e.preventDefault();
+    let email = document.querySelector("#email").value;
+    let password = document.querySelector("#password").value;
     signInWithEmailAndPassword(auth, email, password)
     .then((response) => console.log("User Logged In") )
     .catch((err) => console.log(err.message) )
 }
-let email = "abhi@abhi.com"
-let password = "12345678"
 
-signIn(email, password)
+
+googleButton.addEventListener("click", googleLogin)
+
+function googleLogin(){
+    signInWithPopup(auth, provider).then((result) => console.log("User Logged In") )
+    .catch((err) => console.log(err.message) )
+}
+
+
--- pages-js/SignUp.js
+++ pages-js/SignUp.js
@@ -1,21 +1,37 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import firebaseApp from "../firebase.js";
+
+
+
+let signUpForm = document.querySelector(".signup-form"); 
+
+
+signUpForm.addEventListener("submit", signUp)
 
 
 const auth = getAuth(firebaseApp);
 
-function signUp(email, password){
+function signUp(e){
+   console.log("SignUp process started")
+   e.preventDefault();
+   let email = document.querySelector("#email").value; 
+   let password = document.querySelector("#password").value;
    createUserWithEmailAndPassword(auth,email, password)
-   .then(success)
+   .then(user => {
+      console.log("User signup successfull")
+      updateProfile(auth.currentUser, {
+         displayName: document.querySelector("#name").value
+      })
+      .then(
+         () => console.log("User Info Updated")
+      )
+      .catch(err => console.log(err.message) )
+      
+      })
    .catch(err => console.log(err.message) )
 
-   function success(response){
-    console.log("User Created Sucessfully")
-   }
+   
    
     
 }
 
-let email = "abhi4@abhi.com"
-let password = "12345678"
-signUp(email, password)
